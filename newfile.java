c78f12394a8d4259cc30541aca072e27cdd913e1
ssarf459 <94968573+ssarf459@users.noreply.github.com>
2022-05-12 11:29:08 +0530

    Add files via upload

---
+++ newfile.java
@@ -0,0 +1,12 @@
+// Jvdroid-main: 
+// Jvdroid-main: 
+// Jvdroid-main: 
+// Jvdroid-main: 
+public class Main {
+	public static void Main(String[] args) {
+		int i=100, j=200;
+		while(++i<--j){
+		    System.out.println(i);
+		}
+	}
+}
