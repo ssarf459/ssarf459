5018ba6b8e41f77091b5e624ae6490642a5a2efd
ssarf459 <94968573+ssarf459@users.noreply.github.com>
2023-03-13 20:54:30 +0530

    Delete Personal directory

--- Personal/js/index.js
+++
@@ -1,9 +0,0 @@
-function funct(){
-    var input_string = document.getElementById("input_string").value;
-    if (input_string === "Syed") {
-        document.write("Hello " + input_string);
-    }
-    else{
-        window.alert("You put the incorrect data");
-    }
-}
