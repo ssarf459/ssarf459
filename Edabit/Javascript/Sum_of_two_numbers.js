53982c4296b17a8121552a2c3026574d6af35c7c
Syed_Sarfaraj_Al_Sahi <94968573+ssarf459@users.noreply.github.com>
2024-10-05 09:37:52 +0530

    correction of mistakes

--- Edabit/Javascript/Sum_of_two_numbers.js
+++ Edabit/Javascript/Sum_of_two_numbers.js
@@ -1,6 +1,6 @@
 function sum_of_two_numbers(a, b){
     sum = a + b;
-    console.log("Sum of the ${a} and ${b} is ${sum}");
+    console.log(`Sum of the ${a} and ${b} is ${sum}`);
 }
 
 sum_of_two_numbers(4, 5)
